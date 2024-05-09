8b06d31c687f7d471f76ffeb38599ca318eea713
dudu-soliveira <116734258+dudu-soliveira@users.noreply.github.com>
2024-05-09 12:32:31 -0300

    feat(model): adiciona as possiveis tags e a interface para os dados

---
+++ model/interfaceData.ts
@@ -0,0 +1,9 @@
+interface Data {
+  id: number;
+  nome: string;
+  peso: number;
+  valor: number;
+  quantidade: number;
+  tags: Tags;
+  removido: boolean;
+}
---
+++ model/tags.ts
@@ -0,0 +1,12 @@
+enum Tags {
+  TVs = "TVs",
+  Informatica = "Informática",
+  Smartphones = "Smartphones e tablets",
+  Apple = "Apple",
+  Audio = "Áudio",
+  Games = "Video games",
+  Eletrodomesticos = "Eletrodomésticos",
+  Ventilacao = "Ar e ventilação",
+  Esportes = "Esportes",
+  Domesticos = "Utensílios domésticos",
+}
