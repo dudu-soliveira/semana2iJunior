c588a6231f354eb9970d1644aff14c31c10e149f
dudu-soliveira <116734258+dudu-soliveira@users.noreply.github.com>
2024-05-12 12:28:19 -0300

    feat(CSV): adiciona funcoes writeCSV e readCSV para a interacao com o banco de dados.

---
+++ model/writeCSV.ts
@@ -0,0 +1,17 @@
+import { createObjectCsvWriter as createCsvWriter } from "csv-writer";
+import { Data } from "./interfaceData";
+
+export const writeCSV = async (
+  filePath: string,
+  data: Data[]
+): Promise<void> => {
+  const csvWriter = createCsvWriter({
+    path: filePath,
+    header: [
+      { id: "title", title: "TÍTULO" },
+      { id: "value", title: "VALOR" },
+    ],
+  });
+
+  return csvWriter.writeRecords(data);
+};
